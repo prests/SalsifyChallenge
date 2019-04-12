89132463c57dc3546b78c788bcdb73f3763fe88d
Shayne Preston <prests@rpi.edu>
2019-04-12 14:57:43 -0400

    commenting code

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+/**
+ * @author Shayne F. Preston
+ */
+
+//Importing
 var express = require('express');
 var app = express();
 var fs = require("fs");
 
+//GET Request Handler
 app.get('/lines/:line', function (req, res) {
     setImmediate(function(){
         console.log(req.params.line);
-        if(req.params.line >= fileArr.length){
-            res.sendStatus(413);
+        if(req.params.line >= fileArr.length){ //line outside bounds
+            res.status(413).end("");
         }
-        else
+        else //line inside bounds
         {
             res.status(200).end(fileArr[req.params.line]);
         }
@@ -16,11 +22,12 @@ app.get('/lines/:line', function (req, res) {
 })
 
 var server = app.listen(8082, function () {
-    var host = server.address().address
-    var port = server.address().port
+    var host = server.address().address // listen on 127.0.0.1
+    var port = server.address().port //listen on port 8082
     console.log("Example app listening at http://%s:%s", host, port)
 })
 
+//Read in file and split into array by '\n'
 var text = fs.readFileSync("./SpecialTextFile.txt").toString('ascii');
 var fileArr = text.split('\n');
 console.log(fileArr.length);
